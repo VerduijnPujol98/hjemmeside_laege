62fbddaa29052d79735389be1a00c26c9e298d25
Asuka <asukaleenverduijn@gmail.com>
2025-10-15 15:45:23 +0200

    Indhold: fjern 'Dr.' fra synlige steder (Hero alt); Footer allerede uden 'Dr.'

--- src/app/components/Hero.js
+++ src/app/components/Hero.js
@@ -104,7 +104,7 @@ export default function Hero() {
         <GridCol span={{ base: 12, md: 6 }}>
           <Image
             src="/photos/about_picture.jpg?v=2"
-            alt="Dr. Kristian Høy, PhD - Klinisk lektor, Ortopædkirurgi"
+            alt="Kristian Høy, PhD - Klinisk lektor, Ortopædkirurgi"
             radius="12px"
             h={{ base: 320, md: 520 }}
             style={{
